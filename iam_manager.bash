b786a61e014836ddb1b252dff69fce1ac4557a5c
Michael Whitis <mdwhitis@gmail.com>
2018-04-24 06:26:46 -0700

    Update iam_manager.bash
    mafe it less site specific

--- iam_manager.bash
+++ iam_manager.bash
@@ -1,13 +1,14 @@
 $!/bin/bash
 a cron script that queries IAM every few minutes, and syncs users and public keys.
 # Has a number of requirements including ansible.
-# Version 0.1 on 4/20/2018 by Michael Whitis mdwhitis@gmail.com
+# Version 0.2 on 4/24/2018 by Michael Whitis mdwhitis@gmail.com
 
+AWSPublickey=[Your Key Here]
 rm no-key.log
 aws iam get-group --group-name authorized-ssh-users  | grep UserName | cut -d\" -f4 |
 while IFS= read -r username
 do
-aws  iam get-ssh-public-key --ssh-public-key-id APKAIVF7HHFX7VVWKVJQ --user-name "$username" --encoding SSH
+aws  iam get-ssh-public-key --ssh-public-key-id "$AWSPublickey" --user-name "$username" --encoding SSH
 if [ $? -ne 0 ]
 then echo "$username no SSH iam key">> no-key.log
 else echo "     - $username">> iam_add.yml
